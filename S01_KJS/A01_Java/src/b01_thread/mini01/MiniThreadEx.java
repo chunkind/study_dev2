63e708d4feae7fb6c59fa7468c300fa77a067611
chunkind <chunkind@naver.com>
2025-05-24 09:26:39 +0900

    f

--- S01_KJS/A01_Java/src/b01_thread/mini01/MiniThreadEx.java
+++ S01_KJS/A01_Java/src/b01_thread/mini01/MiniThreadEx.java
@@ -24,18 +24,16 @@ class Worker implements Runnable {
    public void run() {
       while (true) {
          Task task = null;
-         synchronized (MiniThreadEx.workQueue) {
+//         synchronized (MiniThreadEx.workQueue) {
             if (MiniThreadEx.workQueue.size() > 0) {
                task = MiniThreadEx.workQueue.dequeue();
             }
-         }
+//         }
 
          if (task != null) {
             System.out.println(Thread.currentThread().getName() + " 작업 시작");
             task.execute();
             System.out.println(Thread.currentThread().getName() + " 작업 완료");
-         } else {
-            break;
          }
       }
    }

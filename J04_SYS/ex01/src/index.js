15a86a80a3cad7400f5b2949e37f5b45c986e50f
dudtlr2331 <dudtlr102@naver.com>
2025-07-19 23:48:30 +0900

    리액트 예제

---
+++ J04_SYS/ex01/src/index.js
@@ -0,0 +1,6 @@
+import randomColor from "randomcolor";
+import r from "randomcolor";
+
+const color = r();
+
+console.log(color);
---
+++ J04_SYS/ex03/ex03/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button(){
+    return(
+        <button>안녕</button>
+    );
+}
+
+export default Button;

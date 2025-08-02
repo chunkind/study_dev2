33c0190296cfc15765d20b7684b53282cbd5d5b8
chunkind <chunkind@naver.com>
2025-08-02 11:24:56 +0900

    aa

---
+++ J01_KJS/ex04/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Header.module.css';
+
+function Header(){
+  return (
+    <div className={styles.header}>
+      <div>해더1</div>
+    </div>
+  );
+}
+
+export default Header;

09f71cd46f2bfcf465daef762136714ed1100e09
chunkind <chunkind@naver.com>
2025-08-02 11:51:46 +0900

    8/2:

--- J01_KJS/ex04/src/App.jsx
+++ J01_KJS/ex04/src/App.jsx
@@ -1,28 +1,32 @@
 import './App.css';
 import Header from './components/Header';
+import ImageCard from './components/ImageCard';
 import Footer from './components/Footer';
 
 
-/*
-------------------------------------------
------  ----- -----
-|   |  |   | |   |
------  ----- -----
- BO     FO    PO
------  
-|   |  
------  
- BO    
-------------------------------------------
-
-*/
 
 function App() {
+  // 비동기로 데이터를 가지고 온다.
+  let json = {
+    carArr: [
+      {title: 'BO', img : '...', desc:'bo 입니다'},
+      {title: 'FO', img : '...', desc:'fo 입니다'},
+      {title: 'PO', img : '...', desc:'po 입니다'},
+      {title: 'PS', img : '...', desc:'ps 입니다'}
+    ]
+  }
+
+  let dataList = json.carArr;
   return (
     <>
       <Header/>
-      <div>
-        <ImageCard/>
+      <div className='container'>
+        {dataList.map(item => (
+          <ImageCard 
+            title={item.title}
+            desc={item.desc}
+          />
+        ))}
       </div>
       <Footer/>
     </>
--- J01_KJS/ex04/src/components/ImageCard.jsx
+++ J01_KJS/ex04/src/components/ImageCard.jsx
@@ -1,1 +1,16 @@
- 
+import React from 'react';
+import styles from './ImageCard.module.css';
+
+function ImageCard({title, desc}){
+  return (
+    <div className={styles.cardContainer}>
+      <img src=''  />
+      <div>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+ }
+
+ export default ImageCard;
